bc53f4100eda2233630b5c8bdd1214ffd896deab
adsuzk <adsuzk@example.com>
2025-09-11 08:44:34 +0000

    AUTO-SAVE #76 - 2025-09-11_08-44-34

--- sections index/header.php
+++ sections index/header.php
@@ -145,9 +145,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
     
 </head>
 <body>
