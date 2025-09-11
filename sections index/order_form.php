40530e066b24ca668dfb7d78b4ec25262632de0b
adsuzk <adsuzk@example.com>
2025-09-11 06:51:36 +0000

    AUTO-SAVE #37 - 2025-09-11_06-51-36

--- sections index/order_form.php
+++ sections index/order_form.php
@@ -488,13 +488,8 @@
                     <div class="map-container-sticky" id="mapContainerSticky">
                         <div id="map"></div>
                         <div class="map-info-overlay" style="display: none;">
-                            <!-- Overlay masqué par demande utilisateur -->
-                            <!-- <div id="routeInfo" class="route-info" style="display: none;"> -->
-                                <div class="distance-info">Distance: <span id="routeDistance">-</span></div>
-                                <div class="duration-info">Durée: <span id="routeDuration">-</span></div>
-                                <div class="price-info">Prix: <span id="routePrice">-</span></div>
-                            </div>
-                        </div>
+                    <!-- map-info-overlay removed: map price/time overlay is disabled by user request -->
+                <!-- End of disabled map-overlay -->
                     </div>
                 </div>
             </div>
