4c57a3aeb4afd3a67ebb57155558a23ea7c24b01
Suzosky Protection <suzosky@github.com>
2025-09-27 21:27:19 +0000

    Auto-backup 21:27:17 scan 1490

--- debug_coursiers_en_ligne.php
+++ debug_coursiers_en_ligne.php
@@ -3,7 +3,7 @@ require_once 'config.php';
 
 $pdo = getDBConnection();
 $stmt = $pdo->query("
-    SELECT id, nom, prenoms, statut_connexion, current_session_token, last_activity 
+    SELECT id, nom, prenoms, statut_connexion, current_session_token, last_login 
     FROM agents_suzosky 
     WHERE statut_connexion = 'en_ligne'
 ");
