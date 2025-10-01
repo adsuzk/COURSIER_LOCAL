f3dd77e409d74f80036c9e0ec9f8b9c07b98c195
Suzosky Protection <suzosky@github.com>
2025-10-01 05:16:17 +0000

    Auto-backup 05:16:16 scan 5949

--- CoursierAppV7/app/src/main/java/com/suzosky/coursier/ui/components/BottomNavigationBar.kt
+++ CoursierAppV7/app/src/main/java/com/suzosky/coursier/ui/components/BottomNavigationBar.kt
@@ -1,19 +1,29 @@
 package com.suzosky.coursier.ui.components
 
-import androidx.compose.foundation.layout.size
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Chat
-import androidx.compose.material.icons.filled.DeliveryDining
-import androidx.compose.material.icons.filled.Wallet
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.suzosky.coursier.ui.theme.*
 
